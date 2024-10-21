efe7c177f05a738dc09c71617710eda4c013e014
rd2000 <rd2000_git@myitdb.de>
2024-10-21 16:23:56 +0200

    New function and testfile

---
+++ tests/Test-ConvertFrom-TextTable.ps1
@@ -0,0 +1,61 @@
+<# TEST #>
+
+$IncPath = ".\functions\"
+.$IncPath"ConvertFrom-TextTable.ps1"
+
+
+# Beispieltext (die Tabelle als String)
+$text = @"
++-----------+-----------+-------+----------------+-------------------+
+| Vorname   | Nachname  | PLZ   | Ort            | Straße            |
++-----------+-----------+-------+----------------+-------------------+
+| John      | Meier     | 10115 | Berlin         | Hauptstraße 12    |
+| Lisa      | Schmidt   | 80331 | München        | Bahnhofstraße 8   |
+| Michael   | Müller    | 20095 | Hamburg        | Lindenweg 3       |
+| Sarah     | Weber     | 50667 | Köln           | Gartenstraße 22   |
+| David     | Schneider | 01067 | Dresden        | Parkallee 7       |
+| Laura     | Fischer   | 70173 | Stuttgart      | Schulstraße 19    |
+| Markus    | Wolf      | 28195 | Bremen         | Rosenweg 4        |
+| Anna      | Krause    | 55116 | Mainz          | Brunnenstraße 15  |
+| Peter     | Bauer     | 90403 | Nürnberg       | Kirchplatz 2      |
+| Julia     | Zimmermann| 14467 | Potsdam        | Alte Allee 10     |
++-----------+-----------+-------+----------------+-------------------+
+"@
+
+
+# JSON-Definition als String
+$jsonString = @"
+{
+    "tableaddresses": {
+        "removelines": {
+            "header": 3,
+            "footer": 3
+        },
+        "extract": {
+            "Vorname": {
+                "start": 2,
+                "length": 10
+            },
+            "Nachname": {
+                "start": 14,
+                "length": 10
+            },
+            "PLZ": {
+                "start": 26,
+                "length": 6
+            },
+            "Ort": {
+                "start": 34,
+                "length": 15
+            },
+            "Straße": {
+                "start": 51,
+                "length": 18
+            }
+        }
+    }
+}
+"@
+
+$result = ConvertFrom-TextTable -textTable $text -jsonString $jsonString
+$result
