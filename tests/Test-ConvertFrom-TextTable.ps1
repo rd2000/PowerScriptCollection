e15b0022673b9e5f9585f50a40f61754721b15d2
rd2000 <rd2000_git@myitdb.de>
2024-10-24 20:54:51 +0200

    Add new function for creating string hashes. Change tests.

--- tests/Test-ConvertFrom-TextTable.ps1
+++ tests/Test-ConvertFrom-TextTable.ps1
@@ -1,10 +1,11 @@
-<# TEST #>
+<# TEST - Do not change the test, please! #>
 
 $IncPath = ".\functions\"
 .$IncPath"ConvertFrom-TextTable.ps1"
-
+.$IncPath"Get-CustomHash.ps1"
 
 # Example data (The table as string)
+# MD5 hash: 8C1F1B89221E7FE2B827FB7AC8BC4D3D
 $textTable = @"
 +-----------+-----------+-------+----------------+-------------------+
 | Vorname   | Nachname  | PLZ   | Ort            | Straße            |
@@ -24,6 +25,7 @@ $textTable = @"
 
 
 # JSON definition as string
+# MD5 hash: 8DF69827B0B34FF4CD0390A0A9B5467A
 $jsonString = @"
 {
     "tableaddresses": {
@@ -42,5 +44,40 @@ $jsonString = @"
 }
 "@
 
-$result = ConvertFrom-TextTable -textTable $textTable -jsonString $jsonString -mapName "tableaddresses"
-$result
+# The test intro
+Write-Host "This is a test for function: Get-CustomHash.ps1" -ForegroundColor Blue
+
+# First: Check, if the function has been changed since the last test
+if ((Get-FileHash .\functions\ConvertFrom-TextTable.ps1 -Algorithm 'MD5').Hash -ne "92299882A8A6D186C63D816AF8BFC686") {
+    Write-Warning "Function has been modified since the last test!"
+    Write-Host "If test passed, please adjust the checksum of the function to disable this warning."
+}
+
+# Run function
+try {
+
+    $result = ConvertFrom-TextTable -textTable $textTable -jsonString $jsonString -mapName "tableaddresses"
+    $stringresult = $result | Out-String
+
+    # Get hashes from sources and result 
+    $Algorithm = "MD5"
+    $CustomTestSource01 = Get-CustomHash -StringToHash $textTable -HashAlgorithm $Algorithm 
+    $CustomTestSource02 = Get-CustomHash -StringToHash $jsonString -HashAlgorithm $Algorithm 
+    $CustomTestDestination = Get-CustomHash -StringToHash $stringresult -HashAlgorithm $Algorithm 
+
+    # check hashes
+    if (($CustomTestSource01.Hash -eq "8C1F1B89221E7FE2B827FB7AC8BC4D3D") -and ($CustomTestSource02.Hash -eq "8DF69827B0B34FF4CD0390A0A9B5467A")) {
+        if ($CustomTestDestination.Hash -eq "C8F761D85E07562353764B85DECA77D3") {
+            Write-Host "Test passed :-)" -ForegroundColor Green
+        } else {
+            Write-Error "Test failed :-("
+        }
+    } else {
+        Write-Error "Source strings failed" 
+    }
+} 
+
+catch {
+    Write-Error "Error on execution function"
+    Write-Error "Test failed :-("
+}
---
+++ tests/Test-Get-CustomHash.ps1
@@ -0,0 +1,39 @@
+<# TEST - Do not change the test, please! #>
+
+$IncPath = ".\functions\"
+.$IncPath"Get-CustomHash.ps1"
+
+# The test intro
+Write-Host "This is a test for function: Get-CustomHash.ps1" -ForegroundColor Blue
+
+# First: Check, if the function has been changed since the last test
+if ((Get-FileHash .\functions\Get-CustomHash.ps1 -Algorithm 'MD5').Hash -ne "649812437DEE98B6E58F4449D2F5841B") {
+    Write-Warning "Function has been modified since the last test!"
+    Write-Host "If test passed, please adjust the checksum of the function to disable this warning."
+}
+
+# The test string must be "Test-Get-CustomHash"
+# If you change the test string, the test will be fail.
+$TestString = "Test-Get-CustomHash"
+$Algorithm = "MD5"
+$CustomTest = Get-CustomHash -StringToHash $TestString -HashAlgorithm $Algorithm 
+
+# The test hash must be "5111906AF8764152EE4D118B26CE674C"
+# If you change the test hash, the test will be fail.
+$TestHash = "5111906AF8764152EE4D118B26CE674C"
+
+# Test
+if ($CustomTest.Hash -eq $TestHash ) {
+    Write-Host "Test passed :-)" -ForegroundColor Green
+    Write-Host ""
+    Write-Host "Test String:" $TestString
+    Write-Host "Hash:" $CustomTest.Hash
+    Write-Host "Algorithm:" $CustomTest.Algorithm
+} else {
+    Write-Error "Test failed :-("
+    Write-Host "Test String:" $TestString
+    Write-Host "For algorithm:" $CustomTest.Algorithm
+    Write-Host "Hash should be:" $CustomTest.Hash
+    Write-Host "But the hash is:" $TestHash
+}
+
