a31696b48ca4bfcaa1fb59f09ec18a11ad6fa5e2
rd2000 <rd2000_git@myitdb.de>
2024-10-26 18:19:17 +0200

    new script

--- README.md
+++ README.md
@@ -16,6 +16,10 @@ A custom collection of powershell functions, scripts and code snippets.
 [Get Custom Hash](#get-custom-hash)  
 [Get Custom Password](#get-custom-password)  
 
+### Scripts
+
+[WebDav File Downloader](#webdav-file-downloader)  
+
 ---
 
 ## Convert Objects to MSSQL
@@ -23,20 +27,20 @@ A custom collection of powershell functions, scripts and code snippets.
 __A generic function for converting Powershell objects into MSSQL statements.__  
 Convert a PowerShell object to microsoft structured query language. (MSSQL)
 
----
+
 
 ## Join PowerShell Objects
 
-__A simple function to join two PS Objects based on an identic key.__  
+__A simple function to join two PS objects based on an identic key.__  
 The key must exists on both objects. After join returns the two objects as one.
 
----
+
 
 ## Rename NoteProperty of objects
 
 __A function to rename one or multiple NoteProperty of objects.__
 
----
+
 
 ## Convert texttable to object
 
@@ -47,24 +51,30 @@ based on the defined start positions and lengths specified in a JSON string.
 The function removes the specified header lines and returns a list of
 PowerShell objects containing the extracted data.
 
----
+
 
 ## Get Custom Credential
 
 __This function loads a credential, if it does not exist it is created.__
 
----
+
 
 ## Get Custom Hash
 
 __This function create a hash from string.__
 Simple helper, because PowerShell does not provide a cmdlet to compute the hash of a string.
-(https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.utility/get-filehash?view=powershell-7.4)
+[https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.utility/get-filehash?view=powershell-7.4](https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.utility/get-filehash?view=powershell-7.4)
 
----
 
 ## Get Custom Password
 
 __This function loads a custom password, if it does not exist it is created.__
 If commands do not support credentials, this function can be used to provide plain text passwords. 
 The password is still only saved in encrypted form.
+
+---
+
+## WebDav File Downloader
+
+__Power Script Collection - WebDav File Downloader__
+Download images (jpg, png) from WebDav resource and store the images to a local directory
---
+++ scripts/PSC_WebDav_Downloader.ps1
@@ -0,0 +1,89 @@
+﻿<#
+    Power Script Collection - WebDav File Downloader
+
+    .SYNOPSIS
+    Download images (jpg, png) from WebDav resource and store the images to a local directory
+
+    .DESCRIPTION
+    This script download images, or if you want, any filetype from a WebDav resource to a local directory.
+    The script creates a list containing the links to the images on the web resource
+    and loads them only once into the directory.
+
+    .EXAMPLE
+    Set uri to web resource ($uri) and your local destination directory ($dstdir)
+    and then run the script. On the first run, you need your credential. 
+
+#>
+
+# Inlcude functions
+$IncPath = "..\functions\"
+.$IncPath"Get-CustomCredential.ps1"
+
+# File source (webresource)
+$uri = "https://webdav.domain.com/path/to/images/"
+
+# File destination directory
+$dstdir = "$env:userprofile\Pictures\${ResourceName}"
+
+# Filename for Link List
+$ResourceName = "MyWebDavImages"
+$AppDirectory = "PSC_WebDav_File_Downloader"
+
+try {
+    
+    # Get credential
+    $cred = Get-CustomCredential -Path $env:APPDATA"\creds\" -Username "username"
+
+    # Create destination if not exists
+    if (-not ( Test-Path $dstdir ) )
+    {
+        New-Item $dstdir -ItemType Directory
+    }
+
+    # AppDirectory 
+    $LinkListDir = $env:APPDATA + "\" + $AppDirectory
+
+    if ( Test-Path $LinkListDir  ) 
+    {
+        # Get link list from resource file
+        $LinkList = Get-Content -Path "$LinkListDir\$ResourceName"    
+    } 
+    else {
+        # If link list directory for resource not exists,
+        # create them an create a new file with value 0
+        New-Item $LinkListDir -ItemType Directory
+        New-Item "$LinkListDir\$ResourceName" -ItemType File -Value 0
+        $LinkList = Get-Content -Path "$LinkListDir\$ResourceName"  
+    }
+
+    # Get index from resource
+    $req = Invoke-WebRequest -Credential $cred -Uri $uri
+
+    # Only specified file types
+    #$files = $req.Links | Where-Object -Property 'href' -ilike '*.jpg*' 
+    $files = $req.Links | Where-Object { $_.href -ilike '*.jpg*' -or $_.href -ilike '*.png*' }
+        
+    # Download new files
+    $i = 0
+    $files | ForEach-Object { 
+        
+        $filename = $uri + $_.href
+        $destfile = $dstdir + "\" + $_.href
+
+        if (-not ($LinkList.Contains($filename))) {
+            Write-Host "Start Download Source: [$filename] Destination: [$destfile]"
+            Invoke-WebRequest -Credential $cred -Uri $filename  -OutFile $destfile
+            Add-Content -Path "$LinkListDir\$ResourceName" -Value $filename
+            $i++
+        }
+    }
+
+    if ( $i -eq 0 ) {
+        Write-Host "No new files found to download." -ForegroundColor Yellow
+    } else {
+        Write-Host "Downloaded files: ${i}" -ForegroundColor Green
+    }
+}
+catch {
+    Write-Error "An error occurred: $_"
+}
